24fb6ad42e25b7cbfe84bbd2aab8ee8766a98a7d
Mikhail Novikov <mixail.novikov@gmail.com>
2017-03-24 21:37:52 +0300

    add export as nodejs module and as window.check

--- index.js
+++ index.js
@@ -1,78 +1,86 @@
-function argsCheckExactLength(expectedArgumentsLength) {
-  return function(args, checkerName) {
-    if (args.length === expectedArgumentsLength) {
-      return true;
+(function() {
+  function argsCheckExactLength(expectedArgumentsLength) {
+    return function(args, checkerName) {
+      if (args.length === expectedArgumentsLength) {
+        return true;
+      }
+
+      throw new Error('Checker "' + checkerName + '" expect exactly ' + expectedArgumentsLength + ' argument' + (expectedArgumentsLength > 1 ? 's' : '') + ', got ' + args.length);
     }
+  };
 
-    throw new Error('Checker "' + checkerName + '" expect exactly ' + expectedArgumentsLength + ' argument' + (expectedArgumentsLength > 1 ? 's' : '') + ', got ' + args.length);
-  }
-};
+  var checkers = [{
+    name: 'eq',
+    initArgsChecker: argsCheckExactLength(1),
+    check: function(initArgs, checkArgs) {
+      return initArgs[0] === checkArgs[0];
+    }
+  }, {
+    name: 'gt',
+    initArgsChecker: argsCheckExactLength(1),
+    check: function(initArgs, checkArgs) {
+      return initArgs[0] > checkArgs[0];
+    }
+  }, {
+    name: 'lt',
+    initArgsChecker: argsCheckExactLength(1),
+    check: function(initArgs, checkArgs) {
+      return initArgs[0] < checkArgs[0];
+    }
+  }, {
+    name: 'btw',
+    initArgsChecker: argsCheckExactLength(2),
+    check: function(initArgs, checkArgs) {
+      return (initArgs[0] < checkArgs[0]) && (checkArgs[0] < initArgs[1]);
+    }
+  }, {
+    name: 'isNaN',
+    initArgsChecker: argsCheckExactLength(0),
+    check: function(initArgs, checkArgs) {
+      return isNaN(checkerArgs[0]);
+    }
+  }];
 
-var checkers = [{
-  name: 'eq',
-  initArgsChecker: argsCheckExactLength(1),
-  check: function(initArgs, checkArgs) {
-    return initArgs[0] === checkArgs[0];
-  }
-}, {
-  name: 'gt',
-  initArgsChecker: argsCheckExactLength(1),
-  check: function(initArgs, checkArgs) {
-    return initArgs[0] > checkArgs[0];
-  }
-}, {
-  name: 'lt',
-  initArgsChecker: argsCheckExactLength(1),
-  check: function(initArgs, checkArgs) {
-    return initArgs[0] < checkArgs[0];
-  }
-}, {
-  name: 'btw',
-  initArgsChecker: argsCheckExactLength(2),
-  check: function(initArgs, checkArgs) {
-    return (initArgs[0] < checkArgs[0]) && (checkArgs[0] < initArgs[1]);
-  }
-}, {
-  name: 'isNaN',
-  initArgsChecker: argsCheckExactLength(0),
-  check: function(initArgs, checkArgs) {
-    return isNaN(checkerArgs[0]);
-  }
-}];
+  var formatArgs = function(args) {
+    switch (args.length) {
+      case 0:
+        return true;
+      case 1:
+        return args[0];
+      default:
+        return args;
+    }
+  };
 
-var formatArgs = function(args) {
-  switch (args.length) {
-    case 0:
-      return true;
-    case 1:
-      return args[0];
-    default:
-      return args;
-  }
-};
+  var checker = {};
 
-var checker = {};
+  for (const checkerItem of checkers) {
+    Object.defineProperty(checker, checkerItem.name, {
+      get: function() {
+        return function() {
+          var initArgs = Array.prototype.slice.call(arguments);
 
-for (const checkerItem of checkers) {
-  Object.defineProperty(checker, checkerItem.name, {
-    get: function() {
-      return function() {
-        var initArgs = Array.prototype.slice.call(arguments);
+          typeof checkerItem.initArgsChecker === "function" && checkerItem.initArgsChecker(initArgs, checkerItem.name);
 
-        typeof checkerItem.initArgsChecker === "function" && checkerItem.initArgsChecker(initArgs, checkerItem.name);
-
-        return {
-          check: function() {
-            var checkArgs = Array.prototype.slice.call(arguments);
-            return checkerItem.check.call(null, initArgs, checkArgs);
-          },
-          toJSON: function() {
-            return {$check: {[checkerItem.name]: formatArgs(initArgs)}}
+          return {
+            check: function() {
+              var checkArgs = Array.prototype.slice.call(arguments);
+              return checkerItem.check.call(null, initArgs, checkArgs);
+            },
+            toJSON: function() {
+              return {$check: {[checkerItem.name]: formatArgs(initArgs)}}
+            }
           }
-        }
-      };
-    }
-  });
-}
+        };
+      }
+    });
+  }
 
-module.exports = checker;
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = checker;
+  } else if (typeof window !== 'undefined') {
+    window.check = checker;
+  } else {
+    throw new Error('Unknown environment. Cannot be loaded.');
+  }
+})();
